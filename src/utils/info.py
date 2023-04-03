2d2ec22a39fd0e7e69fd487c41056c00a81fdf64
KJongUk <jongwook1103011@gmail.com>
2023-04-03 17:04:33 +0900

    Feat(./src/utils/info.py): Add global datas
    * Add dict for predefined model supported by Pytorch
    
    * Add COCO dataset's labels

---
+++ src/utils/info.py
@@ -0,0 +1,30 @@
+MODEL_WEIGHT = {
+    "fcos_resnet50_fpn": "FCOS_ResNet50_FPN_Weights", 
+    "fasterrcnn_mobilenet_v3_large_320_fpn": "FasterRCNN_MobileNet_V3_Large_320_FPN_Weights", 
+    "fasterrcnn_mobilenet_v3_large_fpn": "FasterRCNN_MobileNet_V3_Large_FPN_Weights", 
+    "fasterrcnn_resnet50_fpn_v2": "FasterRCNN_ResNet50_FPN_V2_Weights", 
+    "fasterrcnn_resnet50_fpn": "FasterRCNN_ResNet50_FPN_Weights", 
+    "retinanet_resnet50_fpn_v2": "RetinaNet_ResNet50_FPN_V2_Weights", 
+    "retinanet_resnet50_fpn": "RetinaNet_ResNet50_FPN_Weights", 
+    "ssd300_vgg16": "SSD300_VGG16_Weights", 
+    "ssdlite320_mobilenet_v3_large": "SSDLite320_MobileNet_V3_Large_Weights", 
+}
+
+labels =[
+    "__background__","person","bicycle","car","motorcycle",
+    "airplane","bus","train","truck","boat","traffic light",
+    "fire hydrant","N/A","stop sign","parking meter","bench","bird",
+    "cat","dog","horse","sheep","cow","elephant",
+    "bear","zebra","giraffe","N/A","backpack","umbrella",
+    "N/A","N/A","handbag","tie","suitcase","frisbee",
+    "skis","snowboard","sports ball","kite","baseball bat","baseball glove",
+    "skateboard","surfboard","tennis racket","bottle","N/A","wine glass",
+    "cup","fork","knife","spoon","bowl","banana",
+    "apple","sandwich","orange","broccoli","carrot","hot dog",
+    "pizza","donut","cake","chair","couch","potted plant",
+    "bed","N/A","dining table","N/A","N/A","toilet",
+    "N/A","tv","laptop","mouse","remote","keyboard",
+    "cell phone","microwave","oven","toaster",
+    "sink","refrigerator","N/A","book","clock","vase",
+    "scissors","teddy bear","hair drier","toothbrush"
+]
