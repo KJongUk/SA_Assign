17229516fa157da0ca51c9e6ca68c92ef40e337c
KJongUk <jw@jongwookui-MacBookAir.local>
2023-04-04 16:10:10 +0900

    Add: global variable for model's input size
    - MODEL_INPUT: dictionary for model and model's input dimension

--- src/utils/info.py
+++ src/utils/info.py
@@ -10,6 +10,18 @@ MODEL_WEIGHT = {
     "ssdlite320_mobilenet_v3_large": "SSDLite320_MobileNet_V3_Large_Weights", 
 }
 
+MODEL_INPUT = {
+    "fcos_resnet50_fpn": None, 
+    "fasterrcnn_mobilenet_v3_large_320_fpn": None, 
+    "fasterrcnn_mobilenet_v3_large_fpn": None, 
+    "fasterrcnn_resnet50_fpn_v2": None, 
+    "fasterrcnn_resnet50_fpn": None, 
+    "retinanet_resnet50_fpn_v2": None, 
+    "retinanet_resnet50_fpn": None, 
+    "ssd300_vgg16": None, 
+    "ssdlite320_mobilenet_v3_large": [320,320], 
+}
+
 labels =[
     "__background__","person","bicycle","car","motorcycle",
     "airplane","bus","train","truck","boat","traffic light",
